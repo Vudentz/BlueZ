7f742c32f2eb3b89360553b82bea4a0af285db67
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2014-02-08 14:15:33 +0200

    core/profile: Add authorization UUID information to btd_profile
    This is necessary since some profiles may use different UUID for
    authorization.

--- profiles/audio/a2dp.c
+++ profiles/audio/a2dp.c
@@ -2035,6 +2035,7 @@ static struct btd_profile a2dp_source_profile = {
 
 	.remote_uuid	= A2DP_SOURCE_UUID,
 	.local_uuid	= A2DP_SINK_UUID,
+	.auth_uuid	= ADVANCED_AUDIO_UUID,
 
 	.device_probe	= a2dp_source_probe,
 	.device_remove	= a2dp_source_remove,
@@ -2054,6 +2055,7 @@ static struct btd_profile a2dp_sink_profile = {
 
 	.remote_uuid	= A2DP_SINK_UUID,
 	.local_uuid	= A2DP_SOURCE_UUID,
+	.auth_uuid	= ADVANCED_AUDIO_UUID,
 
 	.device_probe	= a2dp_sink_probe,
 	.device_remove	= a2dp_sink_remove,
