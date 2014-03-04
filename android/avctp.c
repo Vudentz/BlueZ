9a1068f24c6a43190ad052f95ea61d60c9cbc4ac
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2014-03-04 15:08:17 +0200

    AVCTP: Fix sending wrong response format
    Reject and Not Implemented responses of PASS THROUGH commands shall
    contain the data of the previous frame accourding to AVC Panel Subunit
    specification page 87.

--- android/avctp.c
+++ android/avctp.c
@@ -315,7 +315,7 @@ static size_t handle_panel_passthrough(struct avctp *session,
 
 	if (*code != AVC_CTYPE_CONTROL || *subunit != AVC_SUBUNIT_PANEL) {
 		*code = AVC_CTYPE_REJECTED;
-		return 0;
+		return operand_count;
 	}
 
 	if (operand_count == 0)
@@ -381,7 +381,7 @@ static size_t handle_panel_passthrough(struct avctp *session,
 		DBG("AV/C: unknown button 0x%02X %s",
 						operands[0] & 0x7F, status);
 		*code = AVC_CTYPE_NOT_IMPLEMENTED;
-		return 0;
+		return operand_count;
 	}
 
 done:
