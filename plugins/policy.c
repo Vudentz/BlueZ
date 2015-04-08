b1c186d93733cea2c705284f310e452e171324ee
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-04-08 13:32:22 +0300

    policy: Mark reconnect services
    This make use of btd_service_reconnect to set reconnecting flag of
    services.

--- plugins/policy.c
+++ plugins/policy.c
@@ -512,6 +512,25 @@ static void target_cb(struct btd_service *service,
 	}
 }
 
+static void mark_service(void *data, void *user_data)
+{
+	struct btd_service *service = data;
+	bool value = GPOINTER_TO_UINT(user_data);
+
+	btd_service_reconnect(service, value);
+}
+
+static void reconnect_set_active(struct reconnect_data *reconnect, bool value)
+{
+	if (reconnect->active == value)
+		return;
+
+	reconnect->active = value;
+
+	g_slist_foreach(reconnect->services, mark_service,
+						GUINT_TO_POINTER(value));
+}
+
 static void reconnect_reset(struct reconnect_data *reconnect)
 {
 	reconnect->attempt = 0;
@@ -521,6 +540,8 @@ static void reconnect_reset(struct reconnect_data *reconnect)
 		g_source_remove(reconnect->timer);
 		reconnect->timer = 0;
 	}
+
+	reconnect_set_active(reconnect, false);
 }
 
 static bool reconnect_match(const char *uuid)
@@ -692,13 +713,15 @@ static void reconnect_set_timer(struct reconnect_data *reconnect)
 	reconnect->active = true;
 
 	if (reconnect->attempt < reconnect_intervals_len)
-		timeout = reconnect_intervals[reconnect->attempt];
+		timeout = reconnect_intervals[reconnect->attempt++];
 
 	DBG("attempt %u/%zu %d seconds", reconnect->attempt + 1,
 						reconnect_attempts, timeout);
 
 	reconnect->timer = g_timeout_add_seconds(timeout, reconnect_timeout,
 								reconnect);
+
+	reconnect_set_active(reconnect, true);
 }
 
 static void disconnect_cb(struct btd_device *dev, uint8_t reason)
